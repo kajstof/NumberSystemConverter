7724704a426ed086dbfdb93852780997a09166c9
Krzysztof Krysiak <krysiak86@gmail.com>
2017-07-27 15:18:44 +0200

    First init

---
+++ NumberSystemConverter.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace NumberSystemConverter.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Number_Greater_Than_ThreeThousand_Throws_IndexOutOfRangeException()
+        {
+            var converter = new RomanNumeralConverter();
+            Assert.Throws<IndexOutOfRangeException>(() => converter.ConvertRomanNumeral(3001));
+        }
+
+        [Fact]
+        public void Number_Less_Than_One_Throws_IndexOutOfRangeException()
+        {
+            var converter = new RomanNumeralConverter();
+            converter.ConvertRomanNumeral(-1);
+        }
+    }
+}
---
+++ NumberSystemConverter/RomanNaturalConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NumberSystemConverter
+{
+    public class RomanNumeralConverter
+    {
+        public RomanNumeralConverter()
+        {
+        }
+
+        public void ConvertRomanNumeral(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
