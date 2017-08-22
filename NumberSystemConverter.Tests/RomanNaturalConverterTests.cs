5f7eae3dfa401ab3f2ca64a0ff7a77d7a73df573
Krzysztof Krysiak <krysiak86@gmail.com>
2017-08-22 10:52:29 +0200

    Change from .Net Framework to .Net Standard and .Net Core

--- NumberSystemConverter.Tests/RomanNaturalConverterTests.cs
+++ NumberSystemConverter.Tests/RomanNaturalConverterTests.cs
@@ -1,23 +1,29 @@
 ﻿using System;
-using System.Linq;
 using Xunit;
 
 namespace NumberSystemConverter.Tests
 {
     public class RomanNaturalConverterTests
     {
+        private RomanNumeralConverter _converter;
+
+        private void Execute()
+        {
+            _converter = new RomanNumeralConverter();
+        }
+
         [Fact]
         public void Number_Greater_Than_ThreeThousand_Throws_IndexOutOfRangeException()
         {
-            var converter = new RomanNumeralConverter();
-            Assert.Throws<IndexOutOfRangeException>(() => converter.ConvertRomanNumeral(3001));
+            Execute();
+            Assert.Throws<IndexOutOfRangeException>(() => _converter.ConvertRomanNumeral(3001));
         }
 
         [Fact]
         public void Number_Less_Than_One_Throws_IndexOutOfRangeException()
         {
-            var converter = new RomanNumeralConverter();
-            converter.ConvertRomanNumeral(-1);
+            Execute();
+            _converter.ConvertRomanNumeral(-1);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# DotNetFramework4.7-NumberSystemConverter
-TDD example https://www.codeproject.com/Articles/560137/NET-TDD-Test-Driven-Development-by-example-Part
+# NumberSystemConverter
+[.Net Standard 1.6 + .Net Core 2.0] TDD example https://www.codeproject.com/Articles/560137/NET-TDD-Test-Driven-Development-by-example-Part
